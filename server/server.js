e246b4631d52d0fe591b48590f0daf901530136a
SnowDiles <brahimboulahia59@gmail.com>
2025-07-24 21:24:04 +0200

    [fix] fix lint error

--- server/server.js
+++ server/server.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const app = express();
 const port = 3000;
-const jwt = require("../utils/jwtUtils");
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
