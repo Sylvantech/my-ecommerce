66f0d7bf986f0fecd36ffe5db019982547eed728
Sylvantech <sylvan.rufin@epitech.eu>
2025-07-26 15:21:08 +0200

    feature: add login form

--- client/app/routes.ts
+++ client/app/routes.ts
@@ -3,4 +3,5 @@ import { type RouteConfig, index, route } from "@react-router/dev/routes";
 export default [
   index("routes/_index.tsx"),
   route("register", "routes/register.tsx"),
+  route("login", "routes/login.tsx"),
 ] satisfies RouteConfig;
---
+++ client/app/routes/login.tsx
@@ -0,0 +1,62 @@
+import { useState } from "react";
+import type { ChangeEvent } from "react";
+
+export function meta() {
+  return [
+    { title: "Login - E-commerce" },
+    { name: "description", content: "Login" },
+  ];
+}
+
+interface FormData {
+  email: string;
+  password: string;
+}
+
+export default function Login() {
+  const [formData, setDataForm] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const stateCopy = { ...formData };
+    stateCopy[e.target.name as keyof FormData] = e.target.value;
+    setDataForm(stateCopy);
+  };
+
+  return (
+    <div>
+      <h1 className="flex justify-center p-10 text-2xl">Connexion</h1>
+      <form
+        className="flex flex-col items-center gap-5"
+        onSubmit={e => e.preventDefault()}
+      >
+        <label htmlFor="email">Email</label>
+        <input
+          className="border-gray-300 border p-3 rounded-sm  w-sm"
+          onChange={handleChange}
+          value={formData.email}
+          type="email"
+          name="email"
+          id="email"
+          placeholder="votre@email.com"
+        />
+        <label htmlFor="password">Mot de passe</label>
+        <input
+          className="border-gray-300 border p-3 rounded-sm  w-sm"
+          minLength={6}
+          type="password"
+          name="password"
+          id="password"
+          onChange={handleChange}
+          value={formData.password}
+          placeholder="....."
+        />
+        <button className="mt-10 border bg-black text-white w-sm p-3 font-bold rounded-lg">
+          Se connecter
+        </button>
+      </form>
+    </div>
+  );
+}
