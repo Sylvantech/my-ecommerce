ac1587c69c8e199f362f4d7931ae3d2b4c5c25b8
GusEpitech <augustin.verissimo@epitech.eu>
2025-07-26 13:16:30 +0200

    fix: change the order of method

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -5,6 +5,40 @@ const User = require("../models/User.model");
 const bcrypt = require("bcrypt");
 const jwtUtils = require("../utils/jwtUtils");
 
+router.post("/", async (req, res) => {
+  const { username, email, password } = req.body;
+  const isValid = userUtils.checkInputRegister(req);
+  if (!isValid) {
+    return res.status(400).json({
+      error: "Données invalides",
+    });
+  }
+  try {
+    const newUser = new User({
+      username,
+      email,
+      password,
+    });
+    const user = await newUser.save();
+
+    res.status(201).json({
+      message: "Utilisateur crée avec succès",
+      user: {
+        id: user.id,
+        username: user.username,
+        email: user.email,
+        role: user.role,
+        created_at: user.created_at,
+      },
+    });
+  } catch (error) {
+    return res.status(500).json({
+      error: "Erreur lors de la création de l'utilisateur ",
+      details: error.message,
+    });
+  }
+});
+
 router.get("/", async (req, res) => {
   const { email, password } = req.body;
 
@@ -55,40 +89,6 @@ router.get("/", async (req, res) => {
   } catch (error) {
     return res.status(500).json({
       error: "Erreur lors de l'authentification",
-      details: error.message,
-    });
-  }
-});
-
-router.post("/", async (req, res) => {
-  const { username, email, password } = req.body;
-  const isValid = userUtils.checkInputRegister(req);
-  if (!isValid) {
-    return res.status(400).json({
-      error: "Données invalides",
-    });
-  }
-  try {
-    const newUser = new User({
-      username,
-      email,
-      password,
-    });
-    const user = await newUser.save();
-
-    res.status(201).json({
-      message: "Utilisateur crée avec succès",
-      user: {
-        id: user.id,
-        username: user.username,
-        email: user.email,
-        role: user.role,
-        created_at: user.created_at,
-      },
-    });
-  } catch (error) {
-    return res.status(500).json({
-      error: "Erreur lors de la création de l'utilisateur ",
       details: error.message,
     });
   }
