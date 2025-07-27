1a63d27f1c2c4738901433e301ebf0b8e00d99ba
SnowDiles <brahimboulahia59@gmail.com>
2025-07-27 17:20:46 +0200

    fix: move into a specific document

--- server/server.js
+++ server/server.js
@@ -2,27 +2,9 @@ const express = require("express");
 const app = express();
 const port = 3000;
 const connectDB = require("./config/database");
-const cors = require("cors");
+const corsMiddleware = require("./middleware/corsMiddleware");
 connectDB();
-
-app.use(
-  cors({
-    origin: ["http://localhost:5173"],
-    credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
-    allowedHeaders: [
-      "Content-Type",
-      "Authorization",
-      "Access-Control-Allow-Headers",
-      "Origin",
-      "Accept",
-      "X-Requested-With",
-    ],
-    exposedHeaders: ["Authorization"],
-    preflightContinue: true,
-    optionsSuccessStatus: 204,
-  })
-);
+app.use(corsMiddleware);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
