8d28ef4f59aacf2234aa571fa70171770c601d45
SnowDiles <brahimboulahia59@gmail.com>
2025-07-27 17:07:10 +0200

    fix:implement cors header handling

--- server/server.js
+++ server/server.js
@@ -2,7 +2,27 @@ const express = require("express");
 const app = express();
 const port = 3000;
 const connectDB = require("./config/database");
+const cors = require("cors");
 connectDB();
+
+app.use(
+  cors({
+    origin: ["http://localhost:5173"],
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+    allowedHeaders: [
+      "Content-Type",
+      "Authorization",
+      "Access-Control-Allow-Headers",
+      "Origin",
+      "Accept",
+      "X-Requested-With",
+    ],
+    exposedHeaders: ["Authorization"],
+    preflightContinue: true,
+    optionsSuccessStatus: 204,
+  })
+);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -11,8 +31,6 @@ const userRoutes = require("./routes/userRoutes");
 const adminRoutes = require("./routes/adminRoutes");
 
 app.use("/api/user", userRoutes);
-
-// Routes protégées avec vérification du rôle admin
 app.use("/api/admin", adminRoutes);
 
 app.get("/", (req, res) => {
