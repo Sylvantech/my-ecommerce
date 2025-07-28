719a106eff56843cc13636564b87c048bc255d65
Sylvantech <sylvan.rufin@epitech.eu>
2025-07-28 17:31:06 +0200

    feature: footer in progress

--- client/app/layout/MainLayout.tsx
+++ client/app/layout/MainLayout.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Navbar from "./Navbar";
+import Footer from "./Footer";
 
 type MainLayoutProps = {
   children: React.ReactNode;
@@ -10,6 +11,7 @@ export function MainLayout({ children }: MainLayoutProps) {
     <>
       <Navbar />
       <main>{children}</main>
+      <Footer />
     </>
   );
 }
