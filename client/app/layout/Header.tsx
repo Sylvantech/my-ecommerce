c2db0b4e23651ae8cb1a2b485f917af6ae494b15
Shoiyoo <pierre.decaudin@epitech.eu>
2025-07-29 15:03:55 +0000

    feature: Ajout Police et ombrages

--- client/app/layout/Header.tsx
+++ client/app/layout/Header.tsx
@@ -1,11 +1,11 @@
 export default function Header() {
   return (
     <div className="text-center">
-      <h1 style={{ fontFamily: '"Nunito", sans-serif', fontWeight: 1000 }} className="text-7xl tracking-tight font-bold text-shadow-sm font-baloo -mb-8 mt-6 bg-gradient-to-r from-fuchsia-500 to-cyan-400 bg-clip-text text-transparent">
+      <h1 className="text-7xl font-nunito font-weight: 900 tracking-tight font-bold text-shadow-sm font-baloo -mb-8 mt-6 bg-gradient-to-r from-fuchsia-500 to-cyan-400 bg-clip-text text-transparent">
         Chaussettes
       </h1>
       <br></br>
-      <h1 style={{ fontFamily: '"Nunito", sans-serif', fontWeight: 1000 }} className="text-7xl -mb-5 font-bold text-shadow-lg text-yellow-300 ">Créatives</h1>
+      <h1 className="text-7xl -mb-5 font-weight: 900 text-shadow-lg text-yellow-300 ">Créatives</h1>
       <br></br>
       <p className="text-black font-bold">
         Exprimez votre personnalité avec style et confort
