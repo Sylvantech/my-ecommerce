a2b6df45d9919026310b9789ee375d8e2879e5aa
Shoiyoo <pierre.decaudin@epitech.eu>
2025-07-29 13:17:34 +0000

    feature: Ajout d'un Header

---
+++ client/app/layout/Header.tsx
@@ -0,0 +1,17 @@
+export default function Header() {
+    return (
+      <div className="text-center">
+        <h1 className="text-7xl font-bold -mb-8 mt-6 bg-gradient-to-r from-fuchsia-500 to-cyan-400 bg-clip-text text-transparent">
+            Chaussettes 
+        </h1>
+            <br></br>
+        <h1 className="text-7xl -mb-5 font-bold text-amber-300">
+            Créatives
+        </h1>
+        <br></br>
+        <p className="text-black font-bold">
+            Exprimez votre personnalité avec style et confort
+        </p>
+      </div>
+    );
+  }
