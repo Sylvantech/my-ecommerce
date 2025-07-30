07a74c4ecafbe83c85efcee3d3624a13f91d5921
GusEpitech <augustin.verissimo@epitech.eu>
2025-07-30 21:46:12 +0200

    fix:mainLayout refactor with the last method for display children

--- client/app/layout/MainLayout.tsx
+++ client/app/layout/MainLayout.tsx
@@ -1,18 +1,16 @@
-import React from "react";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import Header from "~/components/Header";
+import { Outlet } from "react-router";
 
-type MainLayoutProps = {
-  children: React.ReactNode;
-};
-
-export function MainLayout({ children }: MainLayoutProps) {
+export function MainLayout() {
   return (
     <div className="min-h-screen font-nunito bg-gradient-to-br from-pink-100 via-purple-50 to-blue-100 relative overflow-hidden">
       <Navbar />
-      <main>{children}</main>
       <Header />
+      <main>
+        <Outlet />
+      </main>
       <Footer />
     </div>
   );
--- client/app/routes/_index.tsx
+++ client/app/routes/_index.tsx
@@ -1,10 +1,6 @@
-import React from "react";
-import Header from "../components/Header";
-
 export default function Index() {
   return (
     <div>
-      <Header />
     </div>
   );
 }
