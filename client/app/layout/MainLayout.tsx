d0478b16cae4df559d3d4e8566b4e28f1a58c5d8
GusEpitech <augustin.verissimo@epitech.eu>
2025-07-30 21:59:03 +0200

    feat: add meta function for page metadata

--- client/app/layout/MainLayout.tsx
+++ client/app/layout/MainLayout.tsx
@@ -3,6 +3,13 @@ import Footer from "../components/Footer";
 import Header from "~/components/Header";
 import { Outlet } from "react-router";
 
+export function meta() {
+  return [
+    { title: "E-commerce" },
+    { name: "description", content: "Site d'e-commerce de chaussette" },
+  ];
+}
+
 export function MainLayout() {
   return (
     <div className="min-h-screen font-nunito bg-gradient-to-br from-pink-100 via-purple-50 to-blue-100 relative overflow-hidden">
