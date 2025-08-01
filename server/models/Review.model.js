2fcc708db62fdd9d5fa1c30e2b8e59df6dddbacb
SnowDiles <brahimboulahia59@gmail.com>
2025-08-01 10:21:52 +0200

    feat:implement review model

---
+++ server/models/Review.model.js
@@ -0,0 +1,40 @@
+const mongoose = require("mongoose");
+const Counter = require("./Counter.model");
+
+const reviewSchema = new mongoose.Schema(
+  {
+    id: {
+      type: Number,
+      unique: true,
+    },
+    user_id: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+    product_id: { type: mongoose.Schema.Types.ObjectId, ref: "Product" },
+    rating: {
+      type: int,
+      required: true,
+      min: 1,
+      max: 5,
+    },
+  },
+  {
+    timestamps: {
+      createdAt: "created_at",
+      updatedAt: "updated_at",
+    },
+  }
+);
+
+reviewSchema.pre("save", async function (next) {
+  if (this.isNew) {
+    const counter = await Counter.findByIdAndUpdate(
+      { _id: "reviewId" },
+      { $inc: { sequence_value: 1 } },
+      { new: true, upsert: true }
+    );
+    this.id = counter.sequence_value;
+  }
+  next();
+});
+
+const Review = mongoose.model("Review", reviewSchema);
+module.exports = Review;
