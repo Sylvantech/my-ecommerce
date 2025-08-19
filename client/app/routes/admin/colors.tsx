10a8d191b14d9a5f7d4070d9f183415107891227
Sylvantech <sylvan.rufin@epitech.eu>
2025-08-19 14:40:03 +0200

    feature: page and component are functional

--- client/app/routes/admin/colors.tsx
+++ client/app/routes/admin/colors.tsx
@@ -1,145 +1,140 @@
 import { useEffect, useState } from "react";
-import CategoryAdmin from "~/components/Admin/CategoryAdmin";
-import { adminService } from "~/services/adminService";
+import ColorsAdmin from "~/components/Admin/ColorsAdmin";
 
 export default function Categories() {
-    const [searchCategory, setSearchCategory] = useState("");
+  const [searchColor, setSearchColor] = useState("");
 
-    const [description, setDescription] = useState("");
+  const [description, setDescription] = useState("");
 
-    const [name, setName] = useState("");
+  const [name, setNameColor] = useState("");
 
-    const [isModalOpen, setIsModalOpen] = useState(false);
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
-    const [refresh, setRefresh] = useState(0);
+  const [color, setColor] = useState("");
 
-    const handleSubmit = async () => {
-        if (name && description) {
-            const res = await adminService.createCategory(name, description);
-            if (res?.success) {
-                setIsModalOpen(false);
-                setRefresh(r => r + 1);
-            } else {
-                return res;
-            }
-        }
-    };
+  const handleColor = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchColor(e.target.value);
+  };
 
-    const handleCategory = (e: React.ChangeEvent<HTMLInputElement>) => {
-        setSearchCategory(e.target.value);
-    };
-
-    return (
-        <div className="sm:ml-80 mr-3 flex flex-col gap-3">
-            {isModalOpen && (
-                <div className="fixed inset-0 flex justify-end items-center p-4 bg-black/50 w-full">
-                    <div className="bg-white w-full max-w-sm p-6 flex flex-col gap-4 rounded-lg">
-                        <div>
-                            <div className="flex justify-end">
-                                <button
-                                    onClick={() => setIsModalOpen(false)}
-                                    className="text-sm text-gray-700"
-                                >
-                                    X
-                                </button>
-                            </div>
-                            <div className="gap-3 flex flex-col">
-                                <h1 className="text-xl">Ajouter une nouvelle couleur</h1>
-                                <hr className="text-gray-300" />
-                                <p className="text-gray-500 text-sm">
-                                    Créez une nouvelle couleur pour vos chaussettes.
-                                </p>
-                            </div>
-                        </div>
-                        <div>
-                            <h3>Nom de la couleur</h3>
-                            <input
-                                className="shadow border border-gray-300 w-full rounded-lg p-1.5"
-                                type="text"
-                                name="name"
-                                id="name"
-                                placeholder="Ex: Rouge Passion"
-                                onChange={e => setName(e.target.value)}
-                            />
-                        </div>
-                        <h3>Code couleur (Hex)</h3>
-                        <div>
-                        <div>
-                            <input type="text" name="" id=""  />
-                        </div>
-                        </div>
-                        <div>
-                            <h3>Description</h3>
-                            <textarea
-                                className="shadow p-2 border border-gray-300 w-full rounded-lg"
-                                name="description"
-                                id="description"
-                                placeholder="Description de la catégorie..."
-                                value={description}
-                                onChange={e => setDescription(e.target.value)}
-                            />
-                        </div>
-                        <div className="flex justify-end gap-3">
-                            <button
-                                onClick={() => setIsModalOpen(false)}
-                                className="border border-gray-200 p-2 rounded-lg text-sm"
-                            >
-                                Annuler
-                            </button>
-                            <button
-                                onClick={handleSubmit}
-                                className="bg-gray-400 hover:bg-black text-white p-2 rounded-lg text-sm"
-                            >
-                                Ajouter
-                            </button>
-                        </div>
-                    </div>
-                </div>
-            )}
-            <h1 className="text-3xl">Gestion des Couleurs</h1>
-            <p className="text-gray-700 mb-6">
-                Gérez la palette de couleurs de vos chaussettes
-            </p>
-            <div className="flex justify-between gap-2">
-                <div className="flex-1 flex flex-row items-center border rounded-xl border-gray-200 gap-4 p-1">
-                    <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        width="24"
-                        height="24"
-                        viewBox="0 0 24 24"
-                        fill="none"
-                        stroke="currentColor"
-                        strokeWidth="2"
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        className="text-gray-400 h-4 w-4"
-                    >
-                        <circle cx="11" cy="11" r="8"></circle>
-                        <path d="m21 21-4.3-4.3"></path>
-                    </svg>
-                    <form action="" className="text-sm w-full">
-                        <label htmlFor=""></label>
-                        <input
-                            type="text"
-                            name=""
-                            id=""
-                            placeholder="Rechercher une couleur..."
-                            className="w-full"
-                            onChange={handleCategory}
-                            value={searchCategory}
-                        />
-                    </form>
-                </div>
+  return (
+    <div className="sm:ml-80 mr-3 ml-3 flex flex-col gap-3">
+      {isModalOpen && (
+        <div className="fixed inset-0 flex justify-end items-center p-4 bg-black/50 w-full">
+          <div className="bg-white w-full max-w-sm p-6 flex flex-col gap-4 rounded-lg">
+            <div>
+              <div className="flex justify-end">
                 <button
-                    onClick={() => setIsModalOpen(true)}
-                    className="bg-black text-white text-sm p-1 rounded-lg flex justify-center items-center gap-1"
+                  onClick={() => setIsModalOpen(false)}
+                  className="text-sm text-gray-700"
                 >
-                    <span className="text-xl">+</span> Ajouter une couleur
+                  X
                 </button>
+              </div>
+              <div className="gap-3 flex flex-col">
+                <h1 className="text-xl">Ajouter une nouvelle couleur</h1>
+                <hr className="text-gray-300" />
+                <p className="text-gray-500 text-sm">
+                  Créez une nouvelle couleur pour vos chaussettes.
+                </p>
+              </div>
             </div>
-            <div className="flex flex-col items-center justify-center">
-                <CategoryAdmin searchCategory={searchCategory} refresh={refresh} />
+            <div>
+              <h3>Nom de la couleur</h3>
+              <input
+                className="shadow border border-gray-300 w-full rounded-lg p-1.5"
+                type="text"
+                name="name"
+                id="name"
+                placeholder="Ex: Rouge Passion"
+                onChange={e => setNameColor(e.target.value)}
+              />
             </div>
+            <h3>Code couleur (Hex)</h3>
+            <div className="flex gap-2 items-center">
+              <div className="border border-gray-300 p-2 w-20 bg-white flex justify-center items-center rounded-sm">
+                <div
+                  className="px-7 py-3 border border-gray-500 "
+                  style={{ backgroundColor: color }}
+                ></div>
+              </div>
+              <input
+                name={color}
+                id={color}
+                className="border border-gray-300 p-1.5 rounded-lg"
+                type="text"
+                value={color}
+                onChange={e => setColor(e.target.value)}
+              />
+            </div>
+            <div>
+              <h3>Description</h3>
+              <textarea
+                className="shadow p-2 border border-gray-300 w-full rounded-lg"
+                name="description"
+                id="description"
+                placeholder="Description de la couleur..."
+                value={description}
+                onChange={e => setDescription(e.target.value)}
+              />
+            </div>
+            <div className="flex justify-end gap-3">
+              <button
+                onClick={() => setIsModalOpen(false)}
+                className="border border-gray-200 p-2 rounded-lg text-sm"
+              >
+                Annuler
+              </button>
+              <button className="bg-gray-400 hover:bg-black text-white p-2 rounded-lg text-sm">
+                Ajouter
+              </button>
+            </div>
+          </div>
         </div>
-    );
+      )}
+      <h1 className="text-3xl">Gestion des Couleurs</h1>
+      <p className="text-gray-700 mb-6">
+        Gérez la palette de couleurs de vos chaussettes
+      </p>
+      <div className="flex justify-between gap-2">
+        <div className="flex-1 flex flex-row items-center border rounded-xl border-gray-200 gap-4 p-1">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            className="text-gray-400 h-4 w-4"
+          >
+            <circle cx="11" cy="11" r="8"></circle>
+            <path d="m21 21-4.3-4.3"></path>
+          </svg>
+          <form action="" className="text-sm w-full">
+            <label htmlFor=""></label>
+            <input
+              type="text"
+              name=""
+              id=""
+              placeholder="Rechercher une couleur..."
+              className="w-full"
+              onChange={handleColor}
+              value={searchColor}
+            />
+          </form>
+        </div>
+        <button
+          onClick={() => setIsModalOpen(true)}
+          className="bg-black text-white text-sm p-1 rounded-lg flex justify-center items-center gap-1"
+        >
+          <span className="text-xl">+</span> Ajouter une couleur
+        </button>
+      </div>
+      <div className="flex flex-col items-center justify-center">
+        <ColorsAdmin searchColor={searchColor} />
+      </div>
+    </div>
+  );
 }
---
+++ client/app/services/admin/adminService.ts
@@ -0,0 +1,158 @@
+import type { User } from "../../types/userlist";
+import { CookieHelper } from "../../utils/cookieHelper";
+export const adminService = {
+  async authenticated(token: string) {
+    try {
+      const res = await fetch("http://localhost:3000/api/admin/", {
+        method: "GET",
+        headers: {
+          Authorization: `Bearer ${token}`,
+          "Content-Type": "application/json",
+        },
+      });
+
+      if (!res.ok) {
+        if (res.status === 401) {
+          throw new Error("Token invalide ou expiré");
+        } else if (res.status === 403) {
+          throw new Error("Droits administrateur requis");
+        } else {
+          throw new Error("Erreur d'authentification");
+        }
+      }
+
+      return await res.json();
+    } catch (error) {
+      throw new Error(
+        error instanceof Error
+          ? error.message
+          : "Erreur inconnue lors de l'authentification"
+      );
+    }
+  },
+
+  getCategories: async () => {
+    try {
+      const res = await fetch("http://localhost:3000/api/category/");
+      if (!res.ok) {
+        return {
+          success: false,
+          error: "Erreur lors de la récupération des catégories",
+        };
+      }
+      const data = await res.json();
+      return { success: true, data: data.categories };
+    } catch (err) {
+      console.error(`erreur: ${err}`);
+      return { success: false, error: "Erreur réseau ou serveur" };
+    }
+  },
+
+  deleteCategory: async (idCategory: number) => {
+    const token = CookieHelper.getToken("AccesToken");
+    if (!token) {
+      return { success: false, error: "Token manquant" };
+    }
+
+    try {
+      const res = await fetch("http://localhost:3000/api/category/", {
+        method: "DELETE",
+        headers: {
+          Authorization: `Bearer ${token}`,
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ id: idCategory }),
+      });
+      if (!res.ok) {
+        if (res.status === 401) {
+          throw new Error("Token invalide ou expiré");
+        } else if (res.status === 403) {
+          throw new Error("Droits administrateur requis");
+        }
+      }
+      return {
+        success: true,
+        data: `La catégorie ${idCategory} a bien été supprimé`,
+      };
+    } catch (err) {
+      console.error(`erreur: ${err}`);
+      return { success: false, error: "Erreur réseau ou serveur" };
+    }
+  },
+
+  getUsers: async (): Promise<{
+    success: boolean;
+    data?: User[];
+    error?: string;
+  }> => {
+    try {
+      const res = await fetch("http://localhost:3000/api/user/");
+      if (!res.ok) {
+        const message = "Erreur lors de la récupération des utilisateurs";
+        return { success: false, error: message };
+      }
+      const data = await res.json();
+      return { success: true, data };
+    } catch (err) {
+      console.error(`erreur: ${err}`);
+      return { success: false, error: "Erreur réseau ou serveur" };
+    }
+  },
+  modifyCategory: async (name: string, description: string, id: number) => {
+    const token = CookieHelper.getToken("AccesToken");
+    if (!token) {
+      return { success: false, error: "Token manquant" };
+    }
+    try {
+      const res = await fetch("http://localhost:3000/api/category/", {
+        method: "PUT",
+        headers: {
+          Authorization: `Bearer ${token}`,
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ name, description, id }),
+      });
+      if (!res.ok) {
+        if (res.status === 401) {
+          throw new Error("Token invalide ou expiré");
+        } else if (res.status === 403) {
+          throw new Error("Droits administrateur requis");
+        }
+      }
+      const data = await res.json();
+      return { success: true, data };
+    } catch (err) {
+      console.error(`erreur: ${err}`);
+      return { success: false, error: "Erreur réseau ou serveur" };
+    }
+  },
+
+  createCategory: async (name: string, description: string) => {
+    const token = CookieHelper.getToken("AccesToken");
+    if (!token) {
+      return { success: false, error: "Token manquant" };
+    }
+    try {
+      const res = await fetch("http://localhost:3000/api/category/", {
+        method: "POST",
+        headers: {
+          Authorization: `Bearer ${token}`,
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ name, description }),
+      });
+      if (!res.ok) {
+        if (res.status === 401) {
+          throw new Error("Token invalide ou expiré");
+        } else if (res.status === 403) {
+          throw new Error("Droits administrateur requis");
+        }
+      }
+      const data = await res.json();
+      return { success: true, data };
+    } catch (err) {
+      console.error(`erreur: ${err}`);
+      return { success: false, error: "Erreur réseau ou serveur" };
+    }
+  },
+};
