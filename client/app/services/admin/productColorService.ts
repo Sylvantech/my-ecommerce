09320856c861c2e52495f526a8959e86508457db
Sylvantech <sylvan.rufin@epitech.eu>
2025-08-20 11:17:38 +0200

    feature: add method for create color

--- client/app/services/admin/productColorService.ts
+++ client/app/services/admin/productColorService.ts
@@ -14,4 +14,26 @@ export const productColor = {
       console.error(err);
     }
   },
+  createColor: async (name: string, hex_code: string) => {
+    try {
+      const res = await fetch("http://localhost:3000/api/productColor", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ name, hex_code }),
+      });
+
+      if (!res.ok) {
+        if (res.status === 401) {
+          throw new Error("Token invalide ou expiré");
+        } else if (res.status === 403) {
+          throw new Error("Droits administrateur requis");
+        }
+      }
+
+      const data = await res.json();
+      return { success: true, data };
+    } catch (err) {
+      return { success: false, error: err };
+    }
+  },
 };
